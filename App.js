718a9f10a5856489490d24d1d92fa673867027d5
Shuaib Alameen <shuaibayomide96@gmail.com>
2026-04-29 12:22:32 +0100

    app.js

--- App.js
+++ App.js
@@ -1,20 +1,32 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+// App.js
+import { NavigationContainer } from "@react-navigation/native"
+import { createStackNavigator } from "@react-navigation/stack"
+import Welcome from "./app/index"
+import RoleSelect from "./app/role-select"
+import Home from "./app/home"
+import ScanArtist from "./app/scan-artist"
+import ScanCollector from "./app/scan-collector"
+import Dashboard from "./app/dashboard"
+import Artwork from "./app/artwork"
+import Profile from "./app/profile"
+import KYC from "./app/kyc"
+
+const Stack = createStackNavigator()
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    <NavigationContainer>
+      <Stack.Navigator screenOptions={{ headerShown: false }}>
+        <Stack.Screen name="index" component={Welcome} />
+        <Stack.Screen name="role-select" component={RoleSelect} />
+        <Stack.Screen name="home" component={Home} />
+        <Stack.Screen name="scan-artist" component={ScanArtist} />
+        <Stack.Screen name="scan-collector" component={ScanCollector} />
+        <Stack.Screen name="dashboard" component={Dashboard} />
+        <Stack.Screen name="artwork" component={Artwork} />
+        <Stack.Screen name="profile" component={Profile} />
+        <Stack.Screen name="kyc" component={KYC} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  )
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
